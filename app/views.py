b797d4a47133cbdb79623f3ed50fdf3fba4af7fe
Arseniy Kushnir <arseniykushnir2007@gmail.com>
2026-08-08 22:05:40 +0300

    The use of threads and templates was configured

--- app/views.py
+++ app/views.py
@@ -5,8 +5,14 @@ from app.tasks import scrap_data
 
 
 def view_scraped_data(request):
-    ...
+    return render(request, "view_scraped_data.html")
 
 
 def start_scrap_data(request):
-    ...
+    scrap = threading.Thread(
+        target=scrap_data,
+        daemon=True,
+    )
+    scrap.start()
+
+    return render(request, "scraping_process_started.html")
--- server/urls.py
+++ server/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from app.views import view_scraped_data
+from app.views import view_scraped_data, start_scrap_data
 
 urlpatterns = [
     path("", view_scraped_data, name="scraped_data_view"),
+    path("scrap/", start_scrap_data, name="scrap_data_view"),
 ]
