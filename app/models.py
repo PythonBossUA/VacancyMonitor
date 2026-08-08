3e8ee89ae347dedf972ea0a4c184a9249450c600
Arseniy Kushnir <arseniykushnir2007@gmail.com>
2026-08-08 21:31:19 +0300

    Models for vacancies have been written

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False, unique=True)
+
+
+class Vacancy(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    publication_date = models.DateField(null=False, blank=False)
+    url = models.URLField(null=False, blank=False)
